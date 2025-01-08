318e313465d9d70e0099c9c45db0b83abc3ea73e
CHAMATH <chamathupeka13@gmail.com>
2025-01-08 22:21:43 +0530

    change the mobile and screen modes

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -55,65 +55,67 @@ const Stats = () => {
           })}
         </div>
       </div>
-      {/* Divider */}
-      <div className="flex justify-center mt-12">
-        <div className="border-t border-accent opacity-50 animate-reversedWidth"></div>
-      </div>
 
-      {/* Service Section */}
-      <div className="mt-8">
-        <h2 className="text-4xl font-bold text-accent mb-6 text-center mt-2">
-          My Services
-        </h2>
-        <ServicePage />
-      </div>
-
-      <div className="mt-4 lg:mt-12">
-        <ResumePage />
-      </div>
-
-      {/* Work Section */}
-      <div className="mt-4 lg:mt-8 ">
-        <div className="flex justify-center mb-4">
-          <div className="border-t border-accent opacity-50 animate-reversedWidth w-full"></div>
+      <div className="block xl:hidden">
+        {/* Divider */}
+        <div className="flex justify-center mt-12">
+          <div className="border-t border-accent opacity-50 animate-reversedWidth"></div>
+        </div>
+        {/* Service Section */}
+        <div className="mt-8">
+          <h2 className="text-4xl font-bold text-accent mb-6 text-center mt-2">
+            My Services
+          </h2>
+          <ServicePage />
         </div>
 
-        <h2 className="text-4xl font-bold text-accent mb-6 text-center ">
-          My Work
-        </h2>
-        <WorkPage />
-      </div>
+        <div className="mt-4 lg:mt-8">
+          <ResumePage />
+        </div>
 
-      {/* Divider */}
-      <div className="flex justify-center ">
-        <div className="border-t opacity-50 animate-reversedWidth"></div>
-      </div>
+        {/* Work Section */}
+        <div className="mt-4 lg:mt-8 ">
+          <div className="flex justify-center mb-4">
+            <div className="border-t border-accent opacity-50 animate-reversedWidth w-full"></div>
+          </div>
 
-      {/* Add a <style> tag for @keyframes */}
-      <style jsx>{`
-        @keyframes reversedWidth {
-          0% {
-            width: 50%;
+          <h2 className="text-4xl font-bold text-accent mb-6 text-center ">
+            My Work
+          </h2>
+          <WorkPage />
+        </div>
+
+        {/* Divider */}
+        <div className="flex justify-center ">
+          <div className="border-t opacity-50 animate-reversedWidth"></div>
+        </div>
+
+        {/* Add a <style> tag for @keyframes */}
+        <style jsx>{`
+          @keyframes reversedWidth {
+            0% {
+              width: 50%;
+            }
+            50% {
+              width: 40%;
+            }
+            100% {
+              width: 30%;
+            }
           }
-          50% {
-            width: 40%;
-          }
-          100% {
-            width: 30%;
-          }
-        }
 
-        .animate-reversedWidth {
-          animation: reversedWidth 5s infinite ease-in-out;
-        }
-      `}</style>
+          .animate-reversedWidth {
+            animation: reversedWidth 5s infinite ease-in-out;
+          }
+        `}</style>
 
-      {/* Contact Section */}
-      <div className="mt-8 mb-3">
-        <h2 className="text-4xl font-bold text-accent mb-6 text-center ">
-          Contact Me
-        </h2>
-        <ContactPage />
+        {/* Contact Section */}
+        <div className="mt-8 mb-3">
+          <h2 className="text-4xl font-bold text-accent mb-6 text-center ">
+            Contact Me
+          </h2>
+          <ContactPage />
+        </div>
       </div>
     </section>
   );
